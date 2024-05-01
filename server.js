f72de001d4e877ae716d0098ddb219ecc3e11766
azannoor <azaanoor18@.com>
2024-05-01 08:09:59 -0700

    update:"update server and env file"

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import notFoundMiddleware from './middlewares/NotFound.js'
+import mongoose from 'mongoose'
 
 const app = express()
 
@@ -10,9 +11,20 @@ const app = express()
 app.get("/",(req,res)=>{
     res.send('welcome')
 })
-//middleware
-app.use(notFoundMiddleware)
+// //middleware
+// app.use(notFoundMiddleware)
 
-app.listen(5000,()=>{
-    console.log('server listen')
-})
+
+
+const start = async()=>{
+    try{
+        await mongoose.connect("mongodb+srv://azaanoor18:5meORaMKvsKeojwP@hello.ngmfanm.mongodb.net/?retryWrites=true&w=majority&appName=Hello")
+        app.listen(5000,()=>{
+            console.log('server listen')
+        })
+    } catch(error){
+        console.log(error)
+    }
+}
+
+start()
